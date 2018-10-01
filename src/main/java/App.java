06bf3e5db93fc6b8ba0f95b762d9dd731cdd1de9
Denis Kalinin <k-d-s@mail.ru>
2018-10-01 21:40:29 +0300

    Pars data.

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,5 +1,18 @@
+import by.en.service.pars.ParsLesson;
+
+import java.io.FileNotFoundException;
+import java.util.Map;
+
 public class App {
     public static void main(String[] args) {
-
+        ParsLesson pars = new ParsLesson();
+        Map<Integer, String> lessons = null;
+        try {
+            lessons = pars.getMurphyLesson();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+        assert lessons != null;
+        lessons.forEach((integer, s) -> System.out.println(integer + " " + s));
     }
 }
---
+++ src/main/java/by/en/service/pars/ParsLesson.java
@@ -0,0 +1,55 @@
+package by.en.service.pars;
+
+import java.io.BufferedInputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.Scanner;
+import java.util.stream.Collectors;
+
+public class ParsLesson {
+
+    private Integer count=0;
+
+    public Map<Integer, String>  getMurphyLesson() throws FileNotFoundException {
+
+        File file = new File("murphy.txt");
+
+        Scanner scanner = new Scanner(new BufferedInputStream(new FileInputStream(file)));
+        scanner.useDelimiter("\"Unit");
+
+        List<String> units = new ArrayList<>();
+
+        while (scanner.hasNext()) {
+            String line = scanner.next();
+            units.add(line.substring(0,150));
+        }
+        scanner.close();
+
+        return units
+                .stream()
+                .map(s->s.split("\"}")[0].trim())
+                .map(s->s.replace(".", " "))
+                .map(s -> s.replace("  ", " "))
+                .filter(this::deleteTheSame)
+                .collect(Collectors.toMap(c->Integer.valueOf(c.split("\\s")[0].trim()), c->c.replaceFirst("(\\d*? )", " ").trim()));
+    }
+
+    private boolean deleteTheSame(String str){
+
+        int number = Integer.valueOf(str.split(" ")[0].trim());
+
+        if (number != count + 1){
+            return false;
+        }
+
+        if (number > count) {
+            count++;
+            return true;
+        }
+        return false;
+    }
+}
